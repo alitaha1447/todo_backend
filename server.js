5055582d26a5ede99bbc913510ee8e6582ebdf53
alitaha1447 <ali.taha1447@gmail.com>
2025-03-17 14:45:24 +0530

    add versel.json5

--- server.js
+++ server.js
@@ -6,20 +6,17 @@ import connectDb from "./src/config/dbConfig.js";
 connectDb();
 
 // ROUTERS
-// import authRouter from "./src/routers/authRouters.js";
-// import taskRouter from "./src/routers/taskRouters.js";
-// import userRouter from "./src/routers/userRouters.js";
+import authRouter from "./src/routers/authRouters.js";
+import taskRouter from "./src/routers/taskRouters.js";
+import userRouter from "./src/routers/userRouters.js";
 
 var app = express();
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.use("/api/v1/auth", authRouter);
-// app.use("/api/v1/jobs", taskRouter);
-// app.use("/api/v1/user", userRouter);
-app.use("/", (req, res) => {
-  res.send("Hello Server versel");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", taskRouter);
+app.use("/api/v1/user", userRouter);
 
 const PORT = process.env.PORT || 5200;
 
